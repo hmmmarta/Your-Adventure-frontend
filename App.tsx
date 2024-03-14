2db67926104b837c57708fe2c5c455f901188715
hmmmarta <gryshechko05@gmail.com>
2024-03-14 15:52:36 +0200

    Sign Up

--- App.tsx
+++ App.tsx
@@ -1,21 +1,45 @@
+import {useFonts, Inter_700Bold, Inter_100Thin} from '@expo-google-fonts/inter';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+
+
 export default function App() {
+  const [fontsLoades] = useFonts({
+    Inter_100Thin,
+    Inter_700Bold
+  });
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.fontInterBold}>Sign up</Text>
+      <Text style={styles.fontInterThin}>Sign up to start Your Adventure</Text>
+      <StatusBar style="auto"/>
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#1F1F2F',
   },
+  fontInterBold: {
+    marginTop: 89,
+    marginLeft: 25,
+    color: "white",
+    fontSize: 40,
+    fontFamily: "Inter_700Bold"
+  },
+  fontInterThin: {
+    fontFamily: "Inter_100Thin",
+    color: "#969696",
+    marginTop: 4,
+    marginLeft: 25,
+    fontSize: 20
+  }
 });
+
