0f22e22acab87c04ea7fb3bbbbe7d2e6507c3695
hmmmarta <gryshechko05@gmail.com>
2024-03-19 17:04:51 +0200

    fonts

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+const styles = StyleSheet.create({
+  buttonPlacement: {
+    flex: 1,
+    marginTop: 80,
+    marginHorizontal: 25,
+  },
+  button: {
+    backgroundColor: "#367AFF",
+    padding: 10,
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+    fontSize: 18,
+    fontFamily: "Inter_600SemiBold",
+  },
+  textInputFocused: {
+    borderColor: "#367AFF",
+  },
+});
+export default styles;
