a7b13c78bdf77a956e2a23c341eb8c247444bd75
hmmmarta <gryshechko05@gmail.com>
2024-03-19 11:06:02 +0200

    foldered

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-//import { StatusBar } from 'expo-status-bar';
 import { View} from 'react-native';
 import SignUp from './src/screens/SignUp/SignUp';
 import background from './src/styles/globalStyles';
